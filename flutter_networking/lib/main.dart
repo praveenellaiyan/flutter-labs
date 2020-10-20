ff0549755687842f2202bf2ecea3da99e1184900
Praveen Ellaiyan <praveen.ellaiyan@quintiles.com>
2020-10-20 19:59:33 +0530

    update application theme

--- flutter_networking/lib/main.dart
+++ flutter_networking/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
