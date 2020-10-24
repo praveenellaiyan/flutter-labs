d61505400e78358e6714744cb2cbdfee4deb92db
Praveen Ellaiyan <praveen.ellaiyan@quintiles.com>
2020-10-24 19:32:45 +0530

    animating on network load

--- flutter_animation/lib/main.dart
+++ flutter_animation/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation/src/user.dart';
 import 'package:flutter_animation/src/users_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   final usersBloc = UsersBloc();
@@ -41,11 +42,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        leading: LoadingContent(widget.usersBloc.isLoading),
       ),
       body: StreamBuilder<UnmodifiableListView<User>>(
         stream: widget.usersBloc.users,
@@ -55,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.account_circle), title: Text("1-5 Users")),
@@ -68,6 +72,9 @@ class _MyHomePageState extends State<MyHomePage> {
           } else if (index == 1) {
             widget.usersBloc.userType.add(UserType.TopUser);
           }
+          setState(() {
+            _currentIndex = index;
+          });
         },
       ),
     );
@@ -77,6 +84,53 @@ class _MyHomePageState extends State<MyHomePage> {
     return ExpansionTile(
       title: Text(user.name),
       subtitle: Text(user.company.name),
+    );
+  }
+}
+
+class LoadingContent extends StatefulWidget {
+  final Stream<bool> isLoading;
+
+  LoadingContent(this.isLoading);
+
+  @override
+  _LoadingContentState createState() => _LoadingContentState(isLoading);
+}
+
+class _LoadingContentState extends State<LoadingContent>
+    with SingleTickerProviderStateMixin {
+  final Stream<bool> isLoading;
+  AnimationController _controller;
+
+  _LoadingContentState(this.isLoading);
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<bool>(
+      stream: isLoading,
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (snapshot.hasData && snapshot.data) {
+          /*return CircularProgressIndicator(
+            backgroundColor: Colors.white,
+          );*/
+          _controller.forward().then((value) => _controller.reverse());
+          return FadeTransition(
+              child: Icon(FontAwesomeIcons.truckLoading),
+              opacity: Tween(begin: 0.5, end: 1.0).animate(
+                  CurvedAnimation(curve: Curves.easeIn, parent: _controller)));
+        } else {
+          return Container();
+        }
+      },
     );
   }
 }
